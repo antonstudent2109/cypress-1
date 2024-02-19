7cb079cb893c3f2c53cb289bc1d863e786dbcd13
antonstudent2109 <159119634+antonstudent2109@users.noreply.github.com>
2024-02-19 14:13:55 +0100

    Second task from homework about hamburger checks

---
+++ cypress/e2e/saucedemo/HamburgerCheck.cy.ts
@@ -0,0 +1,30 @@
+import { STANDART_USER, PASSWORD, URL, LOCKED_USER, PROBLEM_USER } from "../../Constants";
+describe('Login to saucedemo', () => {
+    beforeEach(() => {
+        cy.visit(URL)
+    })})
+    it('Positive login to saucedemo', () => {
+        cy.get('[data-test="username"]').type(STANDART_USER)
+        cy.get('[data-test="password"]').type(PASSWORD)
+    it('All items check', ()=>{
+cy.get('react-burger-menu-btn').click()
+cy.get('.bm-item menu-item').click()
+cy.get('about_sidebar_link').click()
+cy.contains('.MuiTypography-root MuiTypography-body1 css-sere2z')
+    }
+    )})
+    it('Logout check', () => {
+        cy.get('[data-test="username"]').type(STANDART_USER)
+        cy.get('[data-test="password"]').type(PASSWORD) 
+        cy.get('react-burger-menu-btn').click()
+        cy.get('.bm-item menu-item').click()
+        cy.contains('login-button');
+    })
+    it('reset app store', () =>) {
+        cy.get('[data-test="username"]').type(STANDART_USER)
+        cy.get('[data-test="password"]').type(PASSWORD) 
+        cy.get('react-burger-menu-btn').click()
+        cy.get('.bm-item menu-item').click();
+    }
+
+    
