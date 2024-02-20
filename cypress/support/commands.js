9f839939197d0ac2d6845c2a62622a875972ea1c
antonstudent2109 <159119634+antonstudent2109@users.noreply.github.com>
2024-02-20 16:52:09 +0100

    All work

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,11 +5,41 @@ Cypress.Commands.add("validateNamesAtoZ", () => {
         expect($item1).to.have.length(6)
     })
     //cy.get('.inventory_item_name ').should('have.length', 6) //??
-    cy.get('.inventory_item_name ').eq(0).should("have.text", items[0])
-    cy.get('.inventory_item_name ').eq(1).should("have.text", items[1])
-    cy.get('.inventory_item_name ').eq(2).should("have.text", items[2])
-    cy.get('.inventory_item_name ').eq(3).should("have.text", items[3])
-    cy.get('.inventory_item_name ').eq(4).should("have.text", items[4])
+    cy.get('.Sauce Labs Backpack').eq(0).should("have.text", items[0])
+    cy.get('.Sauce Labs Bike Light ').eq(1).should("have.text", items[1])
+    cy.get('.Sauce Labs Bolt T-Shirt').eq(2).should("have.text", items[2])
+    cy.get('.Sauce Labs Fleece Jacket').eq(3).should("have.text", items[3])
+    cy.get('.Sauce Labs Onesie').eq(4).should("have.text", items[4])
+    cy.get('.Test.allTheThings() T-Shirt (Red)').eq(5).should("have.text", items[5])
+    
+})
+
+Cypress.Commands.add("validateNamesZtoA", () => {
+    cy.get('data-test="product_sort_container"').should('have.value', 'za')
+    cy.get('.inventory_item_name ').should(($item1) => {
+        expect($item1).to.have.length(6)
+    })
+    //cy.get('.inventory_item_name ').should('have.length', 6) //??
+    cy.get('.Test.allTheThings() T-Shirt (Red)').eq(0).should("have.text", items[0])
+    cy.get('.Sauce Labs Onesie').eq(1).should("have.text", items[1])
+    cy.get('.Sauce Labs Fleece Jacket').eq(2).should("have.text", items[2])
+    cy.get('.Sauce Labs Bolt T-Shirt ').eq(3).should("have.text", items[3])
+    cy.get('.Sauce Labs Bike Light ').eq(4).should("have.text", items[4])
+    cy.get('.Sauce Labs Backpack ').eq(5).should("have.text", items[5])
+    
+})
+
+Cypress.Commands.add("validateNamesLowToHigh", () => {
+    cy.get('data-test="product_sort_container"').should('have.value', 'lowToHigh')
+    cy.get('.inventory_item_name ').should(($item1) => {
+        expect($item1).to.have.length(6)
+    })
+    //cy.get('.inventory_item_name ').should('have.length', 6) //??
+    cy.get('.Sauce Labs Onesie').eq(0).should("have.text", items[0])
+    cy.get('.Sauce Labs Bike Light').eq(1).should("have.text", items[1])
+    cy.get('.Sauce Labs Bolt T-Shirt ').eq(2).should("have.text", items[2])
+    cy.get('.Test.allTheThings() T-Shirt (Red)').eq(3).should("have.text", items[3])
+    cy.get('.Sauce Labs Backpack').eq(4).should("have.text", items[4])
     cy.get('.inventory_item_name ').eq(5).should("have.text", items[5])
     
 })
